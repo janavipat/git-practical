5332a513daa6596f55eb7eb8e24953222e91d879
janu-patel <118837337+janu-patel@users.noreply.github.com>
2024-01-09 19:27:01 +0530

    function

--- abc.js
+++ abc.js
@@ -1,44 +1,21 @@
-/ program to check if a number is prime or not
+<!DOCTYPE html>
+<html>
+<body>
 
-// take input from the user
-const number = parseInt(prompt("Enter a positive number: "));
-let isPrime = true;
+<h1>JavaScript Functions</h1>
 
-<h2>JavaScript For Loop</h2>
+<p>Call a function which performs a calculation and returns the result:</p>
 
-// check if number is greater than 1
-else if (number > 1) {
+<p id="demo"></p>
 
 <script>
-let text = "";
+let x = myFunction(4, 3);
+document.getElementById("demo").innerHTML = x;
 
-for (let i = 0; i < 5; i++) {
-  text += "The number is " + i + "<br>";
+function myFunction(a, b) {
+  return a * b;
 }
-
-document.getElementById("demo").innerHTML = text;
 </script>
 
 </body>
 </html>
-else if (number > 1) {
-
-    // looping through 2 to number-1
-    for (let i = 2; i < number; i++) {
-        if (number % i == 0) {
-            isPrime = false;
-            break;
-        }
-    }
-
-    if (isPrime) {
-        console.log(`${number} is a prime number`);
-    } else {
-        console.log(`${number} is a not prime number`);
-    }
-}
-
-// check if number is less than 1
-else {
-    console.log("The number is not a prime number.");
-}
